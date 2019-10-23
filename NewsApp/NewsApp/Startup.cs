1a684e823d94da34e3371bc62ed3605996a0cde2
Nguyen Dang Quyet <quyetnd@rikkeisoft.com>
2019-10-23 16:23:50 +0700

    create new project

---
+++ NewsApp/NewsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsApp.Startup))]
+namespace NewsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
