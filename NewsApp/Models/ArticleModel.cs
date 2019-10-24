24a9eef7979c1c8ae39c545ae06bf1c1457e0c33
Do Mi <dotrami78@gmail.com>
2019-10-24 16:37:14 +0700

    show article

---
+++ NewsApp/Models/ArticleModel.cs
@@ -0,0 +1,24 @@
+﻿using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ArticleModel
+    {
+        NewsAppDbContext context = null;
+
+        public ArticleModel()
+        {
+            context = new NewsAppDbContext();
+        }
+
+        public List<Article> ListAll()
+        {
+            return context.Articles.ToList();
+        }
+    }
+}
---
+++ NewsApp/NewsApp/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsApp.Areas.Admin.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Admin/Article
+        public ActionResult Index()
+        {
+            var listArticle = new ArticleModel().ListAll();
+            return View(listArticle);
+        }
+    }
+}
