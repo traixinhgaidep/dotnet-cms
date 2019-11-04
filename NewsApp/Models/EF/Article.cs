136c502620b0aa533592dda83e62cf05f58af7b3
Do Mi <dotrami78@gmail.com>
2019-11-04 17:34:21 +0700

    Sua giao dien trang Edit Article

--- NewsApp/Models/EF/Article.cs
+++ NewsApp/Models/EF/Article.cs
@@ -1,34 +1,40 @@
-namespace Models.EF
+﻿namespace Models.EF
 {
     using System;
-    using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
     using System.Web.Mvc;
 
     public partial class Article
     {
         [Key]
+        [DisplayName("ID báo")]
         public int IDArticle { get; set; }
 
         [StringLength(200)]
+        [DisplayName("Tên báo")]
         public string Title { get; set; }
 
+        [DisplayName("Tên danh mục")]
         public int? IDChannel { get; set; }
 
         [StringLength(100)]
+        [DisplayName("Ảnh")]
         public string Image { get; set; }
 
         [StringLength(500)]
+        [DisplayName("Tóm tắt")]
         public string Summary { get; set; }
 
         [AllowHtml]
+        [DisplayName("Nội dung")]
         public string Content { get; set; }
 
         [StringLength(50)]
+        [DisplayName("Tác giả")]
         public string Author { get; set; }
 
+        [DisplayName("Ngày đăng")]
         public DateTime? Date { get; set; }
     }
 }
